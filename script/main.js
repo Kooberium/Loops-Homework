d71213980f3ae3dec1713b11019c90c09b783951
Kooberium <kooberium@gmail.com>
2022-12-15 11:06:21 +0200

    Final Result

---
+++ script/main.js
@@ -0,0 +1,71 @@
+
+
+
+/////////// Завдання - 1  ///////////////
+const arr = [1, 2, 3, 4, 5];
+
+for ( i = 0; i < arr.length; i++ ) {
+    console.log(arr[i]);
+    document.write(`Output number is - ${arr[i]} <br></br>`);
+};
+
+
+////////// Завдання - 2 ////////////////
+
+// const arr = [2, 3, 4, 5];
+// let result = 1;
+
+// for ( i = 0; i < arr.length; i++ ) {
+//     result *= arr[i];
+// };
+
+// console.log(`Добуток числа - ${result}`);
+
+
+/////////// Завдання - 3 ////////////////
+
+// const obj = {
+//     'мінськ': 'білорусь',
+//     'московія': 'мордор',
+//     'Київ': 'Україна',
+// };
+
+// for ( let capital in obj ) {
+//     console.log(`${capital} - це ${obj[capital]}`);
+// };
+
+
+//////// Завдання - 4 ///////////
+
+// const arr = [1, 2, 3, 4, 5];
+// let result = 0;
+
+// for ( let i = 0; i < arr.length; i++ ) {
+//     result += arr[i];
+// }
+
+// console.log( result );
+
+//////// Завдання - 5 /////////
+// const arr = [2, 5, 9, 15, 0, 4];
+
+// for ( let i = 0; i < arr.length; i++ ) {
+//     let num = arr[i];
+//     if ( num > 3 && num < 10 ) {
+//         console.log(num);
+//         document.write(`Результат числа - ${num} <br></br>`);
+//     }
+// }
+
+////// Завдання - 6/////
+
+// const arr = [1, 2, 5, 9, 4, 13, 4, 10];
+
+// for ( let i = 0; i < arr.length; i++ ) {
+//     let num = arr[i];
+//     if ( num === 4 ) {
+//         console.log('Є');
+//         document.write('Є');
+//         break;
+//     }
+// }
